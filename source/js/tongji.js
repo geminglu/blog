b49bef5e5b7edb579b4f2af5ac9a10eb532ff3e8
minglu_ge <geminglu199698@outlook.com>
2023-10-01 10:47:24 +0800

    部署埋点

---
+++ source/js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fbd317f644ec13b73825f09800d0ea0b";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
